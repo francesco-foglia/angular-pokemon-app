f2b689c306c40c8d6bd7a5c9e93e91ddbb54cefa
Francesco Foglia <francescofogliajj@gmail.com>
2022-10-22 23:40:28 +0200

    modified:   src/app/services/auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  getUser(): string {
+    return localStorage.getItem('username');
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('username') !== null;
+  }
+
+  login(username: string, password: string): boolean {
+    if (username === 'user' && password === 'password') {
+      localStorage.setItem('username', username);
+      return true;
+    }
+    return false;
+  }
+
+  logout(): void {
+    localStorage.removeItem('username');
+  }
 }
