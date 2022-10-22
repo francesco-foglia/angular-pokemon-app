adc6c794b65251320169949d6fbe30dfb6b8de8e
Francesco Foglia <francescofogliajj@gmail.com>
2022-10-22 22:36:03 +0200

    modified:   src/app/pokemon/pokemon.component.html 	modified:   src/app/pokemon/pokemon.component.ts 	modified:   src/app/search/search.component.html

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-pokemon',
@@ -6,8 +7,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
+  detail: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.detail = {
+      abilities: [],
+      height: 7,
+      name: 'bulbasaur',
+      weight: 69,
+      sprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      stats: [
+        {
+          base_stat: 45,
+          effort: 0,
+          stat: {
+            name: 'hp',
+            url: 'https://pokeapi.co/api/v2/stat/1/'
+          }
+        },
+        {
+          base_stat: 49,
+          effort: 0,
+          stat: {
+            name: 'attack',
+            url: 'https://pokeapi.co/api/v2/stat/2/'
+          }
+        },
+        {
+          base_stat: 49,
+          effort: 0,
+          stat: {
+            name: 'defense',
+            url: 'https://pokeapi.co/api/v2/stat/3/'
+          }
+        },
+        {
+          base_stat: 65,
+          effort: 1,
+          stat: {
+            name: 'special-attack',
+            url: 'https://pokeapi.co/api/v2/stat/4/'
+          }
+        },
+        {
+          base_stat: 65,
+          effort: 0,
+          stat: {
+            name: 'special-defense',
+            url: 'https://pokeapi.co/api/v2/stat/5/'
+          }
+        },
+        {
+          base_stat: 45,
+          effort: 0,
+          stat: {
+            name: 'speed',
+            url: 'https://pokeapi.co/api/v2/stat/6/'
+          }
+        }
+      ]
+    };
+  }
 
   ngOnInit(): void {
   }
