bdf9f60c9d2c64b7e88920b550e91f53c913aa19
Francesco Foglia <francescofogliajj@gmail.com>
2022-10-22 22:04:56 +0200

    modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	modified:   src/app/services/pokemon.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
+import {PokemonService} from './services/pokemon.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    PokemonComponent
+    PokemonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    PokemonService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPokemon(id: string = '1') {
+    
+  }
 }
