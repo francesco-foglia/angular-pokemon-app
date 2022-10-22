3610a09e5e04f74c0d5787419ba0ecc2625cf8c1
Francesco Foglia <francescofogliajj@gmail.com>
2022-10-22 22:41:42 +0200

    modified:   src/app/home/home.component.html 	modified:   src/app/home/home.component.ts 	modified:   src/app/search/search.component.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  results: any[];
 
-  constructor() { }
+  constructor() {
+    this.results = [
+      {name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/'},
+      {name: 'charmander', url: 'https://pokeapi.co/api/v2/pokemon/4/'},
+      {name: 'squirtle', url: 'https://pokeapi.co/api/v2/pokemon/7/'},
+      {name: 'pidgey', url: 'https://pokeapi.co/api/v2/pokemon/16/'}
+    ];
+  }
 
   ngOnInit(): void {
   }
